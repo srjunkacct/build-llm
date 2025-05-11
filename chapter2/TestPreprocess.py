00299b0006fa9ce596b1bc0b82e6336db95d2cea
Edward Lee <edward.lee@gmail.com>
2025-05-11 19:06:06 -0400

    Make path platform-independent

--- chapter2/TestPreprocess.py
+++ chapter2/TestPreprocess.py
@@ -1,11 +1,15 @@
+import os
 import unittest
 
 from Preprocess import preprocess
+from pathlib import Path
+import os
 
 
 class TestPreprocess(unittest.TestCase):
     def testPreprocess(self):
-        with open("/Users/edwardlee/git/build-llm/chapter2/the-verdict.txt", "r", encoding="utf-8") as f:
+        home = str(Path.home())
+        with open(os.path.join(home,"git", "build-llm", "chapter2","the-verdict.txt"), "r", encoding="utf-8") as f:
             text = f.read()
         tokens = preprocess(text)
         self.assertEqual(4690, len(tokens))
