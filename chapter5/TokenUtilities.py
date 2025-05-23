b8bdd0f9654ff02e6de18bfbf5594634764ea33c
Edward Lee <edward.lee@gmail.com>
2025-05-22 21:49:39 -0400

    Begin chapter 5

---
+++ chapter5/TokenUtilities.py
@@ -0,0 +1,36 @@
+import torch
+import tiktoken
+from chapter4.GenerateTextUtils import generate_text_simple
+from chapter4.GPTModel import GPTModel
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+
+GPT_CONFIG_124M={ "vocab_size": 50257,
+                  "context_length": 256,
+                  "emb_dim": 768,
+                  "n_heads": 12,
+                  "n_layers": 12,
+                  "drop_rate": 0.1,
+                  "qkv_bias": False }
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.eval()
+start_context = "Every effort moves you"
+tokenizer = tiktoken.get_encoding("gpt2")
+
+token_ids = generate_text_simple(model=model,
+                                 idx=text_to_token_ids(start_context, tokenizer),
+                                 max_new_tokens=10,
+                                 context_size=GPT_CONFIG_124M["context_length"])
+
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
