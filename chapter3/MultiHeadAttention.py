cb24aa026400bf596a653ebd9a8a7386ccda7550
Edward Lee <edward.lee@gmail.com>
2025-06-05 14:05:59 -0400

    More chapter 5

--- chapter3/MultiHeadAttention.py
+++ chapter3/MultiHeadAttention.py
@@ -13,6 +13,7 @@ class MultiHeadAttention(nn.Module):
         self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
         self.dropout = nn.Dropout(dropout)
         self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
--- chapter5/TokenUtilities.py
+++ chapter5/TokenUtilities.py
@@ -21,6 +21,13 @@ GPT_CONFIG_124M={ "vocab_size": 50257,
                   "drop_rate": 0.1,
                   "qkv_bias": False }
 
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25}
+}
+
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 model.eval()
