5bdaae17d47c1c5f3ba37f9b97e4731ed090e222
Edward Lee <edward.lee@gmail.com>
2025-06-05 15:41:37 -0400

    Update chapter6

--- chapter6/DataUtilities.py
+++ chapter6/DataUtilities.py
@@ -2,6 +2,7 @@ import urllib.request
 import zipfile
 import os
 from pathlib import Path
+import pandas as pd
 
 url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
 zip_path = "sms_spam_collection.zip"
@@ -20,6 +21,21 @@ def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
     original_file_path = Path(extracted_path) / "SMSSpamCollection"
     os.rename(original_file_path, data_file_path)
     print(f"File downloaded and saved as {data_file_path}")
+
+def create_balanced_dataset(df):
+    num_spam = df[df["Label"] == "spam"].shape[0]
+    ham_subset = df[df["Label"] == "ham"].sample( num_spam, random_state=123)
+    balanced_df = pd.concat([ham_subset, df[df["Label"] == "spam"]])
+    return balanced_df
+
+def random_split(df, train_frac, validation_frac):
+    df = df.sample(frac=1, random_state=123).reset_index(drop=True)
+    train_end = int(len(df) * train_frac)
+    validation_end = train_end + int( len(df) * validation_frac)
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+    return train_df, validation_df, test_df
 
 if __name__ == "__main__":
     download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
